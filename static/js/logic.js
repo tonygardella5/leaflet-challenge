c313c5a967128fe9c6439eabc43fbd3ecb1a2d21
Tony Gardella <Tony@Tonys-MacBook-Pro.local>
2020-10-16 23:24:15 -0500

    push

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,6 +1,3 @@
-// We create the tile layer that will be the background of our map.
-console.log("Step 1 working");
-
 var apiKey = "YOUR API KEY HERE!";
 
 var graymap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
@@ -11,24 +8,14 @@ var graymap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{
   id: "mapbox/streets-v11",
   accessToken: apiKey
 });
-
-// We create the map object with options.
 var map = L.map("mapid", {
   center: [
     40.7, -94.5
   ],
   zoom: 3
 });
-
-// Then we add our 'graymap' tile layer to the map.
 graymap.addTo(map);
-
-// Here we make an AJAX call that retrieves our earthquake geoJSON data.
 d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson", function(data) {
-
-  // This function returns the style data for each of the earthquakes we plot on
-  // the map. We pass the magnitude of the earthquake into two separate functions
-  // to calculate the color and radius.
   function styleInfo(feature) {
     return {
       opacity: 1,
@@ -40,8 +27,6 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
       weight: 0.5
     };
   }
-
-  // This function determines the color of the marker based on the magnitude of the earthquake.
   function getColor(magnitude) {
     switch (true) {
     case magnitude > 5:
@@ -58,9 +43,6 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
       return "#98ee00";
     }
   }
-
-  // This function determines the radius of the earthquake marker based on its magnitude.
-  // Earthquakes with a magnitude of 0 were being plotted with the wrong radius.
   function getRadius(magnitude) {
     if (magnitude === 0) {
       return 1;
@@ -68,26 +50,19 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
 
     return magnitude * 4;
   }
-
-  // Here we add a GeoJSON layer to the map once the file is loaded.
   L.geoJson(data, {
-    // We turn each feature into a circleMarker on the map.
-    pointToLayer: function(feature, latlng) {
+      pointToLayer: function(feature, latlng) {
       return L.circleMarker(latlng);
     },
-    // We set the style for each circleMarker using our styleInfo function.
     style: styleInfo,
-    // We create a popup for each marker to display the magnitude and location of the earthquake after the marker has been created and styled
     onEachFeature: function(feature, layer) {
 layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);}
   }).addTo(map);
 
-  // Here we create a legend control object.
   var legend = L.control({
     position: "bottomright"
   });
 
-  // Then add all the details for the legend
   legend.onAdd = function() {
     var div = L.DomUtil.create("div", "info legend");
 
@@ -101,7 +76,6 @@ layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feat
       "#ea2c2c"
     ];
 
-    // Looping through our intervals to generate a label with a colored square for each interval.
     for (var i = 0; i < grades.length; i++) {
       div.innerHTML +=
         "<i style='background: " + colors[i] + "'></i> " +
@@ -110,6 +84,5 @@ layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feat
     return div;
   };
 
-  // Finally, we our legend to the map.
   legend.addTo(map);
 });
